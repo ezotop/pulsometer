d5e8e9ba8918ac22c791f5b9e52ca66af33896ee
Ezotop <valentinevdovenko@gmail.com>
2020-07-02 23:10:39 +0300

    finished adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 1200,
+        speed: 900,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right.png"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 992,
                 settings: {
                     dots: true,
                     arrows: false
-                }
+            }
             }
         ]
     });
